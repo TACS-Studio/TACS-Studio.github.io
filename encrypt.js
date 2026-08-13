1dfc3c344081d432ee3cde2cf5740e501df68053
TACS-Studio <TACS-Studio@outlook.com>
2026-08-13 21:47:15 +0800

    Delete encrypt.js

--- encrypt.js
+++
@@ -1,6 +0,0 @@
-// encrypt.js
-const SECRET_KEY = 'bill74186_firewall_aes_key';
-// AES加密/解密函数（简化版，可直接用）
-function encrypt(data) { /* 加密逻辑 */ }
-function decrypt(data) { /* 解密逻辑 */ }
-export { encrypt, decrypt };
